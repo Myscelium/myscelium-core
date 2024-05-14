160e9cb64be4343cf8e89eb8f9c68c69af706426
Cristian Camargo <ccf@cdone.com.br>
2024-05-14 12:10:30 -0300

    Implement more methods and functions to the task_table

--- OxidizedMyscelium/src/socket_host/task_manager/manager.rs
+++ OxidizedMyscelium/src/socket_host/task_manager/manager.rs
@@ -1,9 +1,6 @@
-use std::collections::HashMap;
-
-use serde::{Deserialize, Serialize};
-
 use crate::CommandInstructions;
-
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -39,8 +36,45 @@ pub struct NodeTask {
     status: TaskStatus,
 }
 
+extern crate chrono;
+use chrono::{DateTime, TimeZone, Utc};
+
+fn ts_to_float(ts: DateTime<Utc>) -> f64 {
+    return ts.timestamp() as f64 + (ts.timestamp_subsec_nanos() as f64) / 1_000_000_000.0;
+}
+
+fn float_to_ts(f: f64) -> DateTime<Utc> {
+    // Convert f64 back to DateTime<Utc>
+    let seconds = f.trunc() as i64;
+    let nanos = ((f.fract() * 1_000_000_000.0).round() as u32) % 1_000_000_000;
+    let reconstructed_time = Utc.timestamp(seconds, nanos);
+    return reconstructed_time;
+}
+
 impl NodeTask {
-    pub fn new() -> Self {}
+    pub fn new(parity_id: String, command: CommandInstructions) -> Self {
+        let received_ts: f64 = ts_to_float(Utc::now());
+        Self {
+            parity_id,
+            command,
+            received_ts,
+            sended_to_target_ts: -1f64,
+            received_by_target_ts: -1f64,
+            status: TaskStatus::ScheduledToSend,
+        }
+    }
+
+    // Mark task as sended to the target
+    pub fn sended(&mut self) {
+        self.sended_to_target_ts = ts_to_float(Utc::now());
+        self.status = TaskStatus::WaitingReceiveConf;
+    }
+
+    // Mark task as received by the target, current state waiting a response
+    pub fn received_conf(&mut self) {
+        self.received_by_target_ts = ts_to_float(Utc::now());
+        self.status = TaskStatus::WaitingResponse;
+    }
 }
 
 pub struct NodesTaskManager {
@@ -87,5 +121,10 @@ impl NodesTaskManager {
             continue;
         }
         return Err(TaskManagerError::TaskNotFound(node_key.clone(), parity_id.clone()));
+    }
+
+    pub fn remove_task_of_a_node(&mut self, node_key: &String, parity_id: &String) -> Result<&mut NodeTask, TaskManagerError> {
+        let mut tasks = self.get_node_tasks(node_key)?;
+        tasks.retain(|&t| t.parity_id == *parity_id);
     }
 }
