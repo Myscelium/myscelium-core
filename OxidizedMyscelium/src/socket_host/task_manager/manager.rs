66b2e5927dd5247756bdecbbed5ebb9f6e4dd3a4
Cristian Camargo <ccf@cdone.com.br>
2024-05-17 16:28:08 -0300

    Did some progress building the table for sync, it is not implemented yet, but all tests besides the test_inplace_response_redirect are working as intended

--- OxidizedMyscelium/src/socket_host/task_manager/manager.rs
+++ OxidizedMyscelium/src/socket_host/task_manager/manager.rs
@@ -123,8 +123,9 @@ impl NodesTaskManager {
         return Err(TaskManagerError::TaskNotFound(node_key.clone(), parity_id.clone()));
     }
 
-    pub fn remove_task_of_a_node(&mut self, node_key: &String, parity_id: &String) -> Result<&mut NodeTask, TaskManagerError> {
+    pub fn remove_task_of_a_node(&mut self, node_key: &String, parity_id: &String) -> Result<(), TaskManagerError> {
         let mut tasks = self.get_node_tasks(node_key)?;
-        tasks.retain(|&t| t.parity_id == *parity_id);
+        tasks.retain(|t| t.parity_id == *parity_id);
+        Ok(())
     }
 }
