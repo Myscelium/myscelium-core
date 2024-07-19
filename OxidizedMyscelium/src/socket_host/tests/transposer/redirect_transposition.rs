d2fec6dfc11c50c2d9e44e1ce43fd23e9d950e0a
Cristian Camargo <ccf@cdone.com.br>
2024-07-19 16:10:17 -0300

    Add some comments and planning for the integrate tranposition tests

---
+++ OxidizedMyscelium/src/socket_host/tests/transposer/redirect_transposition.rs
@@ -0,0 +1,12 @@
+// TODO >>> The idea of this module is to test the percurse of the tranposer
+
+// -> Basic idea:
+// > Input some command in the buffer down
+// > 
+
+
+// TODO >>> Initialize the buffer
+// TODO >>> Add the command to tranpose in the buffer down
+// TODO >>> Execute the process function of the tranposer
+// TODO >>> Verify if the command was transposed
+// TODO >>> Verify if the command transposed matches the expected
