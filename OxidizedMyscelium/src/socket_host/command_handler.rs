278215c5aa9f6455a10ac89010cb13b86bab3e46
Cristian Camargo <ccf@cdone.com.br>
2025-06-11 01:22:53 -0300

    Fix typos in logging messages within command handler

--- OxidizedMyscelium/src/socket_host/command_handler.rs
+++ OxidizedMyscelium/src/socket_host/command_handler.rs
@@ -185,7 +185,7 @@ pub async fn redirect_commands_processing(command: &Command, target: &String, co
     let logger = acquire_logger!("Core");
     let mut client_key: String = "".to_string();
 
-    logger.debug(format!("[HOST][REGIRSTRED PATTERNS]:\n{:?}", command_patterns)).await;
+    logger.debug(format!("[HOST][REGISTERED PATTERNS]:\n{:?}", command_patterns)).await;
 
     //> PREVIOUSLY CHECK REQUIREMENTS BEFORE REDIRECT
     if !command_patterns.target_is_reachable(target).unwrap() {
@@ -391,13 +391,13 @@ pub async fn host_commands_processing(command: &Command) -> Command {
     let logger = acquire_logger!("Core");
     let mut client_key: String = "".to_string();
 
-    let mut command_patterns;
+    let mut command_patterns: NetworkMap;
 
     {
         command_patterns = HOST_COMMAND_PATTERNS.lock().await.clone();
     }
 
-    logger.debug(format!("[HOST][REGIRSTRED PATTERNS]:\n{:?}", command_patterns)).await;
+    logger.debug(format!("[HOST][REGISTERED PATTERNS]:\n{:?}", command_patterns)).await;
 
     let direct_functions: Vec<String> = vec!["get_registered_commands", "update_client_commands_ref", "restrictive_update_client_commands_ref", "add_client", "update_client", "remove_client"]
         .into_iter()
@@ -414,7 +414,7 @@ pub async fn host_commands_processing(command: &Command) -> Command {
 
     // > VERIFY IF ALREADY PROCESSED:
     logger.debug("Command is in command patterns!".to_string()).await;
-    let command_alread_processed: bool = match enhanced_buffer::buffer_up_manager::check_if_parity_id_is_registered(command.parity_id.clone(), command.client_key.clone()).await {
+    let command_already_processed: bool = match enhanced_buffer::buffer_up_manager::check_if_parity_id_is_registered(command.parity_id.clone(), command.client_key.clone()).await {
         Ok(cminr) => cminr, // TODO >>> Verify if there is a better way to do this error handling!
         Err(e) => {
             let command: Command = create_error_command_response!(command.client_key.clone(), command.parity_id, format!("Error trying to check if parity id is registered: {:?}", &command));
@@ -425,7 +425,7 @@ pub async fn host_commands_processing(command: &Command) -> Command {
     let response: Command;
 
     //> HANDLE COMMANDS WITH RESPONSE:
-    if command_alread_processed {
+    if command_already_processed {
         logger.warn(format!("Command {}, already have a response!", command.parity_id.clone())).await; // TODO >>> Change this error it's incorrect!
         let response = match get_response(command.clone()).await {
             Ok(sc) => {
@@ -467,7 +467,7 @@ pub async fn host_commands_processing(command: &Command) -> Command {
                 command.client_key.clone(),
                 command.parity_id,
                 format!(
-                    "Can't send a response from command: {:?} processed in host to a host response handler: {:?}, this is a self conflic!",
+                    "Can't send a response from command: {:?} processed in host to a host response handler: {:?}, this is a self conflict!",
                     command.command.actf, command.command.response_actf
                 )
             );
